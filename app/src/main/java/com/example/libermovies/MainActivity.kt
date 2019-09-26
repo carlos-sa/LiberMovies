29ee2a3430193d2fd52c7ed11a1006901f8a532e
Carlos sa <carlos.santoandrea@hotmail.com>
2019-09-26 17:21:12 -0300

    adding search widget to list activity

--- app/src/main/java/com/example/libermovies/MainActivity.kt
+++ app/src/main/java/com/example/libermovies/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.libermovies
 
+import android.app.SearchManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +14,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Create menu items
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.search_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // Handle Menu items click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.search_button -> {
+                onSearchButton()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    // Search Button handler fuction, calls search widget
+    private fun onSearchButton() {
+        println("On Search Requested")
+        onSearchRequested()
+    }
+
+    // Receive new intents to this activity
+    override fun onNewIntent(intent: Intent) {
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    // Deal with received intents
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            doSearch(intent.getStringExtra(SearchManager.QUERY))
+        }
+    }
+
+    // Handle search queries
+    private fun doSearch(query: String) {
+        // TODO: Insert here code to search on omdbapi
+    }
+
 }
